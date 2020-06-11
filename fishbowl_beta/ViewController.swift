837b33ebbbd71a8075818b05473ad86347d32893
AlecShunnarahSC <alec@saturnergonomics.com>
2020-06-10 22:40:30 -0500

    Initial Commit

---
+++ fishbowl_beta/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  fishbowl_beta
+//
+//  Created by Alec Shunnarah on 6/10/20.
+//  Copyright © 2020 Alec Shunnarah. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ fishbowl_betaTests/fishbowl_betaTests.swift
@@ -0,0 +1,36 @@
+//
+//  fishbowl_betaTests.swift
+//  fishbowl_betaTests
+//
+//  Created by Alec Shunnarah on 6/10/20.
+//  Copyright © 2020 Alec Shunnarah. All rights reserved.
+//
+
+import XCTest
+@testable import fishbowl_beta
+
+class fishbowl_betaTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
